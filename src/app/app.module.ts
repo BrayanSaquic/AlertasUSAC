647c850b10e113cb965831f0ea745d6c125907b0
Marlon Rodriguez <brayanmunioz1313@gmail.com>
2018-10-15 21:08:02 -0600

    Primer Cambion en pantallas Login, Registro y Vehiculos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+//Tabs
+import { ObjPerdidosPage } from '../pages/obj-perdidos/obj-perdidos';
+import { ObjVehiculosPage } from '../pages/obj-vehiculos/obj-vehiculos';
+import { AlertasClasePage } from '../pages/alertas-clase/alertas-clase';
 
+//Paginas
+import { RegistroUsuarioPage } from '../pages/registro-usuario/registro-usuario';
+import { LogueoAplicativoPage } from '../pages/logueo-aplicativo/logueo-aplicativo';
+
+import { SecVehiculoIngresoPage } from '../pages/sec-vehiculo-ingreso/sec-vehiculo-ingreso';
+import { SecVehiculoListadoPage } from '../pages/sec-vehiculo-listado/sec-vehiculo-listado';
+import { SecVehiculoRegistroPage } from '../pages/sec-vehiculo-registro/sec-vehiculo-registro';
+
+//otros
+import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    ObjPerdidosPage,
+    ObjVehiculosPage,
+    AlertasClasePage,
+    RegistroUsuarioPage,
+    LogueoAplicativoPage,
+    SecVehiculoIngresoPage,
+    SecVehiculoListadoPage,
+    SecVehiculoRegistroPage,
+
     TabsPage
   ],
   imports: [
@@ -26,9 +44,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    ObjPerdidosPage,
+    ObjVehiculosPage,
+    AlertasClasePage,
+    RegistroUsuarioPage,
+    LogueoAplicativoPage,
+    SecVehiculoIngresoPage,
+    SecVehiculoListadoPage,
+    SecVehiculoRegistroPage,
     TabsPage
   ],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { RegistroUsuarioPage } from '../registro-usuario/registro-usuario';
+import { LogueoAplicativoPage } from '../logueo-aplicativo/logueo-aplicativo';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +10,16 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  launcRegistroPage(){
+
+    this.navCtrl.push(RegistroUsuarioPage);
+  }
+
+  launcLogueoPage(){
+
+    this.navCtrl.push(LogueoAplicativoPage);
   }
 
 }
